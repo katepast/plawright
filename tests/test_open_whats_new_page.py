79234e83e7750447321d69fe40b4288272c80b37
katepast <pastbina1992@gmail.com>
2024-03-20 20:03:38 +0100

    Update test_open_whats_new_page.py

--- tests/test_open_whats_new_page.py
+++ tests/test_open_whats_new_page.py
@@ -1,7 +1,5 @@
 import re
-
 from playwright.sync_api import expect
-
 from pages.customer_login_page import CustomerLoginPage
 from pages.home_page import HomePage
 
@@ -10,7 +8,6 @@ def test_open_whats_new_page(set_up):
     page = set_up
     home_page = HomePage(page)
     home_page.click_whats_new_title()
-
     act_res = home_page.heading_title.text_content()
     assert act_res == "What's New", 'Whats New title is absent on the page'
     expect(page).to_have_url(re.compile(".*/what-is-new.html"))
@@ -24,8 +21,5 @@ def test_sign_in(set_up):
     cust_page.set_email("test@gmail.com")
     cust_page.set_password("test123456")
     cust_page.click_sign_in()
-
-    expect(page.get_by_role("alert")).to_contain_text('The account sign-in was incorrect or your account is disabled temporarily')
-
-
-
+    expect(page.get_by_role("alert")).to_contain_text(timeout=400, use_inner_text='The account sign-in was incorrect')
+    
