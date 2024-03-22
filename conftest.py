a13db482face40abbdc6b6cc35682aef2e131028
katepast <pastbina1992@gmail.com>
2024-03-22 16:13:52 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -17,7 +17,6 @@ PASSWORD = os.environ['EMAIL']
 @pytest.fixture()
 def set_up(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
-    browser = playwright.chromium.launch(headless=True,slow_mo=500)
     context = browser.new_context()
     # Open new page and go to our URL
     page = context.new_page()
