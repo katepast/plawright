0d2964ab8dac966202a37aa4f1aee7027e03b7cc
katepast <pastbina1992@gmail.com>
2024-03-22 12:22:40 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,17 +2,12 @@ import os
 import pytest
 from playwright.sync_api import Playwright
 
-import utils.secret_config
 from pages.customer_login_page import CustomerLoginPage
 from pages.home_page import HomePage
 from utils.url import HOME_PAGE_URL
 
-try:
-    EMAIL = os.environ['EMAIL']
-    PASSWORD = os.environ['EMAIL']
-except KeyError:
-    EMAIL = utils.secret_config.EMAIL
-    PASSWORD = utils.secret_config.PASSWORD
+EMAIL = os.environ['EMAIL']
+PASSWORD = os.environ['EMAIL']
 
 
 @pytest.fixture()
